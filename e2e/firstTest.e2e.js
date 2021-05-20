b4fff01cd1d43f011c25bae897ff339f8894c474
janaka <janaka@m-daq.com>
2021-05-20 11:47:21 +0530

    Update test-case

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -24,13 +24,12 @@
 
 describe('Example', () => {
   beforeAll(async () => {
-    await device.launchApp();
-  });
-
-  beforeEach(async () => {
-    // await device.reloadReactNative();
     await device.launchApp({newInstance: true});
   });
+
+  // beforeEach(async () => {
+  // await device.reloadReactNative();
+  // });
 
   it('should have edit text on welcome screen', async () => {
     await expect(element(by.id('edit-text'))).toBeVisible();
